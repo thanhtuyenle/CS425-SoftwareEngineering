c328a713d5cc0badc6c54f37ddb85990eeaf373f
thanhtuyenle <ttle@mum.edu>
2019-08-11 22:31:47 -0500

    change .properties

---
+++ src/main/java/edu/mum/cs/cs425/finalproject/carmanagement/model/Condition.java
@@ -0,0 +1,9 @@
+package edu.mum.cs.cs425.finalproject.carmanagement.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="conditions")
+public class Condition {
+}
---
+++ src/main/java/edu/mum/cs/cs425/finalproject/carmanagement/model/Style.java
@@ -0,0 +1,4 @@
+package edu.mum.cs.cs425.finalproject.carmanagement.model;
+
+public class Style {
+}
