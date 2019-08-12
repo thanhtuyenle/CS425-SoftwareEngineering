e4666cfc0254719a9270f54939011c83d52d8444
thanhtuyenle <ttle@mum.edu>
2019-08-11 22:36:53 -0500

    delete model classes

--- src/main/java/edu/mum/cs/cs425/finalproject/carmanagement/model/Condition.java
+++
@@ -1,9 +0,0 @@
-package edu.mum.cs.cs425.finalproject.carmanagement.model;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table(name ="conditions")
-public class Condition {
-}
--- src/main/java/edu/mum/cs/cs425/finalproject/carmanagement/model/Style.java
+++
@@ -1,4 +0,0 @@
-package edu.mum.cs.cs425.finalproject.carmanagement.model;
-
-public class Style {
-}
