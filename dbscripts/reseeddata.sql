5c65ada34932ed5a41cbfa00968dfed03e855e7c
thanhtuyenle <ttle@mum.edu>
2019-08-15 10:40:41 -0500

    fix reseeddata

--- dbscripts/reseeddata.sql
+++ dbscripts/reseeddata.sql
@@ -14,10 +14,10 @@ INSERT INTO `carmanagementdb`.`users` (`email`, `firstname`, `lastname`, `middle
 VALUES ('admin@mum.edu', 'admin', 'admin', 'admin', '$2a$10$wOvHq0PgfWoYBuMnd4GVcOAcL7Bx43jxnwtLL8fR8YqMKCI.KPkoa', 'admin@mum.edu');
 
 INSERT INTO `carmanagementdb`.`users` (`email`, `firstname`, `lastname`, `middlename`, `password`,`username`) 
-VALUES ('admin@mum.edu', 'admin', 'admin', 'admin', '$2a$10$wOvHq0PgfWoYBuMnd4GVcOAcL7Bx43jxnwtLL8fR8YqMKCI.KPkoa', 'admin@mum.edu');
-
-INSERT INTO `carmanagementdb`.`users` (`email`, `firstname`, `lastname`, `middlename`, `password`,`username`) 
 VALUES ('dealer@mum.edu', 'dealer', 'dealer', 'dealer', '$2a$10$AgYzpl40rMUXwG2mIj2pKeJwBwxzrKOhb0vPvooR2IbxGjDRWrKYq', 'dealer@mum.edu');
+
+INSERT INTO `carmanagementdb`.`users` (`email`, `firstname`, `lastname`, `middlename`, `password`,`username`)
+VALUES ('user@mum.edu', 'user', 'user', 'user', '$2a$10$IWwUPPV5cPJyeMwQ0VL2QedTH9uDwcz05ZGCeO.RXCxgHEsBFqYDS', 'user@mum.edu');
 
 -- Table roles --
 INSERT INTO `carmanagementdb`.`roles` (`id`, `name`) VALUES ('1', 'ROLE_ADMIN');
